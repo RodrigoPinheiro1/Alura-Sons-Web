f63294c81a13dab43f49854df67d71985dfbd308
RodrigoPinheiro1 <rodrigopsps2015@gmail.com>
2023-11-14 11:47:06 -0300

    adicionado condições para codigo

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
-function tocaSom(idElementAudio) {
-    document.querySelector(idElementAudio).play();
+function tocaSom(seletorAudio) {
+    const selector = document.querySelector(seletorAudio);
+
+    if (selector === null) {
+        alert('yameroooo')
+    }
+    else if (selector != null && selector.localName === "audio"){
+
+        selector.play();
+    }
+    else {
+        console.log('deu ruim');
+    }
 
 }
 
@@ -9,9 +20,7 @@ const lista = document.querySelectorAll('.tecla');
 for (let cont = 0; cont < lista.length; cont++) {
 
     const tecla = lista[cont];
-
     const instrumento = tecla.classList[1]; // nome da tecla
-
     const idAudio = `#som_${instrumento}`; //texto dinamico , template
 
 
@@ -19,5 +28,16 @@ for (let cont = 0; cont < lista.length; cont++) {
         tocaSom(idAudio)
     }
 
+    tecla.onkeydown = function (evento) {
+
+        console.log(evento.code ==='Space')
+        if (evento.code ==='Space' || evento.code ==='Enter') {
+            tecla.classList.add('ativa')
+        }
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa')
+    }
 
 }
